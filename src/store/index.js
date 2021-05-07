8013f08400b7c9445dc0c2c0526ce773149b546b
Gerardo Vargas <evc414@gmail.com>
2021-05-07 11:16:52 -0500

    show

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'Vuex desde cero.',
+    nombre: 'Gerardo',
+    apellido: 'Vargas',
+    amigos: ['Ana', 'Pedro'],
   },
   mutations: {
+    agregarAmigo( state, amigo ){
+      state.amigos = [amigo, ...state.amigos]
+    },
+    eliminarAmigo( state, index ){
+      state.amigos.splice( index, 1 );
+    }
   },
   actions: {
+    addamigoAction( context, amigo ){
+      context.commit('agregarAmigo', amigo)
+    },
+    deleteAmigoAction( context, index ){
+      context.commit('eliminarAmigo', index)
+    }
   },
   modules: {
+  },
+  getters: {
+    mensaje( state ){
+      return state.msg
+    },
+    nombreCompleto(state){
+      return `${state.nombre} ${state.apellido}`
+    }
   }
 })
